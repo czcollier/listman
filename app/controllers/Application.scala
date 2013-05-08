e4134413058e0e7aad281377b1d636f41d70f954
Chris Collier <c.collier@bullhorn.com>
2013-05-08 08:54:21 -0400

    several topics
     - more messing w/ aync. mongo library and how to chain calls nicely
     - added authentication controller and views

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,17 +4,40 @@ import play.api._
 import play.api.mvc._
 import play.modules.reactivemongo._
 import play.modules.reactivemongo.json.collection.JSONCollection
+import play.modules.reactivemongo.json.BSONFormats._
 import play.api.libs.json._
 import scala.concurrent.Future
+import play.api.libs.iteratee.Enumeratee
 import reactivemongo.bson.BSONObjectID
 
 object Application extends Controller with MongoController {
 
-  def collection: JSONCollection = db.collection[JSONCollection]("configs")
+  case class BadComponent(n: String) extends RuntimeException {
+    override def getMessage = n
+  }
+  case object BadSession extends RuntimeException
+
+  def cfgs: JSONCollection = db.collection[JSONCollection]("configs")
   def data: JSONCollection = db.collection[JSONCollection]("data")
 
   import models._
+
+  private def jsonSerialize[T](value: Future[T])(implicit tjs : Writes[T]) = {
+    value.map(Json.toJson(_))
+      .map(Ok(_))
+      .recover {
+        case e: BadComponent => NotFound("")
+        case e =>  BadRequest(e.getClass.getName)
+      }
+  }
+
+  private def byId(idVal: String, fieldName: String = "_id") =
+    Json.obj(fieldName -> Json.obj("$oid" -> idVal))
+
   import JsonCodec._
+
+  val stockId = "5182c2cc5977ba5f00365868"
+
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
@@ -22,22 +45,21 @@ object Application extends Controller with MongoController {
 
   def configs(name: String) = Action {
     Async {
-      val res = collection
-        .find(Json.obj("_id" -> Json.obj("$oid" -> "5182c2cc5977ba5f00365868"))).one[Configuration]
-
-      val comp = res.map(r => r.get.components(name))
-
-      for (c <- comp) {
-        data.find(Json.obj("componentId" -> Json.obj("$oid" -> c._id.get)))
-      }
-      comp
-        .map(Json.toJson(_))
-        .map { x =>
-          Ok(x)
-        } recover {
-          case e => e.printStackTrace()
-          BadRequest(e.getMessage)
+      val res = for {
+        cfg <- cfgs.find(byId(stockId)).cursor[Configuration].toList
+        results <- {
+          cfg.headOption match {
+            case None => Future.failed(BadSession)
+            case Some(q) => q.components.get(name) match {
+              case None => Future.failed(BadComponent(name))
+              case Some(c) => data.find(byId(c._id.get.stringify, "componentId"))
+                  .cursor[JsObject].toList
+          }
         }
+      }} yield results
+
+      jsonSerialize(res)
     }
   }
 }
+
--- app/controllers/JsonCodec.scala
+++ app/controllers/JsonCodec.scala
@@ -2,11 +2,11 @@ package controllers
 
 import models._
 import reactivemongo.bson.BSONObjectID
-import play.modules.reactivemongo.json.BSONFormats.BSONObjectIDFormat
 import play.api.libs.json._
 
 object JsonCodec {
   import play.api.libs.json.Json
+  import play.modules.reactivemongo.json.BSONFormats._
 
 //  implicit val objectIdFormat = Format[BSONObjectID](
 //    (__ \ "$oid").read[String].map { s =>
@@ -21,4 +21,5 @@ object JsonCodec {
   implicit val fieldFormat = Json.format[FieldInfo]
   implicit val componentInfoFormat = Json.format[ComponentInfo]
   implicit val configurationFormat = Json.format[Configuration]
+  //implicit val rawFormat = Json.format[Raw]
 }
